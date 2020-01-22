c72fb12f76a584fa8d9ebf9afb8d0c34412d33d2
Anastasiya194 <Anastasiya194@users.noreply.github.com>
2020-01-22 17:03:27 +0400

    добавляет переделанную структуру  gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
-var gulp = require('gulp'),
+// Создаем переменные и кладем в них нужные для работы Gulp зависимости, которые описаны в package.json
+let gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     cleanCSS = require('gulp-clean-css'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
     del = require('del'),
     browserSync = require('browser-sync'),
     fileinclude = require('gulp-file-include');
 
-sass.compiler = require('node-sass'); // Переназначаем компилирование
+sass.compiler = require('node-sass'); //Переназначаем компилирование
 
 //Обработка scss кода
 function scss() {
-  return gulp.src('./src/scss/main.scss')
+  return gulp.src([
+    './src/main-site/scss/components/components.scss',
+    './src/main-site/scss/pages/*.scss'
+  ])
   .pipe(sass())
   .pipe(autoprefixer({
     cascade: false
@@ -25,7 +30,7 @@ function scss() {
 }
 //Обработка js кода
 function js() {
-  return gulp.src('./src/js/*.js')
+  return gulp.src('./src/main-site/js/*.js')
   .pipe(concat('script.js'))
   .pipe(uglify({toplevel: true}))
   .pipe(gulp.dest('./build'))
@@ -33,26 +38,31 @@ function js() {
 }
 //Инклудинг компонентов на страницы и последующая обработка html кода
 function html() {
-  return gulp.src('src/html/pages/*.html')
+  return gulp.src('./src/main-site/html/pages/*.html')
   .pipe(fileinclude({prefix: '@@', basepath: '@file'}))
   .pipe(gulp.dest('./build'))
   .pipe(browserSync.reload({ stream: true }))
 }
 //Обработка шрифтов
 function fonts() {
-  return gulp.src('./src/fonts/*')
+  return gulp.src('./src/main-site/fonts/*')
   .pipe(gulp.dest('./build/fonts'))
   .pipe(browserSync.reload({ stream: true }))
 }
 //Обработка img
 function img() {
-  return gulp.src('./src/image/*')
+  return gulp.src('./src/main-site/image/*')
   .pipe(gulp.dest('./build/img'))
   .pipe(browserSync.reload({ stream: true }))
 }
 //Удалить всё в указанной папке
 function clean() {
   return del(['./build/*'])
+}
+//Добавить доп. сайты в build
+function addOtherSite() {
+  return gulp.src('./src/other-site/*/*')
+  .pipe(gulp.dest('./build'))
 }
 
 //Просматривать файлы
@@ -63,13 +73,13 @@ function watch() {
     }
   });
   //Следить за SCSS файлами
-  gulp.watch('./src/scss/**/*.scss', scss)
+  gulp.watch('./src/main-site/scss/**/*.scss', scss)
   //Следить за JS файлами
-  gulp.watch('./src/js/*.js', js)
+  gulp.watch('./src/main-site/js/*.js', js)
   //При изменении HTML запустить синхронизацию
   gulp.watch([
-    "./src/html/components/*.html",
-    "./src/html/pages/*.html"
+    "./src/main-site/html/components/*.html",
+    "./src/main-site/html/pages/*.html"
   ], html).on('change', browserSync.reload);
 }
 
@@ -78,9 +88,10 @@ gulp.task('js', js);
 gulp.task('html', html);
 gulp.task('fonts', fonts);
 gulp.task('img', img);
+gulp.task('addOtherSite', addOtherSite);
 gulp.task('watch', watch);
 
 //Запуск buld проекта
-gulp.task('build', gulp.series(clean, gulp.parallel(scss, js, html, fonts, img)))
+gulp.task('build', gulp.series(clean, gulp.parallel(scss, js, html, fonts, img, addOtherSite)))
 //Запуск dev проекта
 gulp.task('dev', gulp.series('build', 'watch'));
