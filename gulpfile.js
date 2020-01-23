3b83b4abf723de22eb6301e87c45d3c0d9e0a00b
Anastasiya194 <Anastasiya194@users.noreply.github.com>
2020-01-23 23:28:13 +0400

    Вносит фундаментальные изменения в логику проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,133 @@
-// Создаем переменные и кладем в них нужные для работы Gulp зависимости, которые описаны в package.json
-let gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cleanCSS = require('gulp-clean-css'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    del = require('del'),
-    browserSync = require('browser-sync'),
-    fileinclude = require('gulp-file-include');
+let gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  cleanCSS = require("gulp-clean-css"),
+  uglify = require("gulp-uglify"),
+  concat = require("gulp-concat"),
+  del = require("del"),
+  browserSync = require("browser-sync"),
+  fileinclude = require("gulp-file-include"),
+  fs = require("fs"),
+  path = require("path");
 
-sass.compiler = require('node-sass'); //Переназначаем компилирование
+sass.compiler = require("node-sass"); // Переназначаем компилирование
 
-//Обработка scss кода
-function scss() {
-  return gulp.src([
-    './src/main-site/scss/components/components.scss',
-    './src/main-site/scss/pages/*.scss'
-  ])
-  .pipe(sass())
-  .pipe(autoprefixer({
-    cascade: false
-  }))
-  .pipe(cleanCSS({
-    level: 2
-  }))
-  .pipe(gulp.dest('./build'))
-  .pipe(browserSync.reload({stream: true}))
+// Функция удаление файлов
+async function clean() {
+  await del(["./build/*"]);
 }
-//Обработка js кода
-function js() {
-  return gulp.src('./src/main-site/js/*.js')
-  .pipe(concat('script.js'))
-  .pipe(uglify({toplevel: true}))
-  .pipe(gulp.dest('./build'))
-  .pipe(browserSync.reload({stream: true}))
+// Функция обработки html файлов
+async function html(src, dest) {
+  return await gulp
+    .src(src)
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: path.join(__dirname, "src/components")
+      })
+    )
+    .pipe(gulp.dest(dest))
+    .pipe(browserSync.reload({ stream: true }));
 }
-//Инклудинг компонентов на страницы и последующая обработка html кода
-function html() {
-  return gulp.src('./src/main-site/html/pages/*.html')
-  .pipe(fileinclude({prefix: '@@', basepath: '@file'}))
-  .pipe(gulp.dest('./build'))
-  .pipe(browserSync.reload({ stream: true }))
+// Функция обработки scss файлов
+async function scss(src, dest) {
+  return await gulp
+    .src(src)
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
+    .pipe(
+      cleanCSS({
+        level: 2
+      })
+    )
+    .pipe(gulp.dest(dest))
+    .pipe(browserSync.reload({ stream: true }));
 }
-//Обработка шрифтов
-function fonts() {
-  return gulp.src('./src/main-site/fonts/*')
-  .pipe(gulp.dest('./build/fonts'))
-  .pipe(browserSync.reload({ stream: true }))
+// Функция обработки js файлов
+async function js(src, dest) {
+  return await gulp
+    .src(src)
+    .pipe(concat("script.js"))
+    .pipe(uglify({ toplevel: true }))
+    .pipe(gulp.dest(dest))
+    .pipe(browserSync.reload({ stream: true }));
 }
-//Обработка img
-function img() {
-  return gulp.src('./src/main-site/image/*')
-  .pipe(gulp.dest('./build/img'))
-  .pipe(browserSync.reload({ stream: true }))
+// Функция обработки шрифтов
+async function fonts(src, dest) {
+  return await gulp
+    .src(src)
+    .pipe(gulp.dest(dest))
+    .pipe(browserSync.reload({ stream: true }));
 }
-//Удалить всё в указанной папке
-function clean() {
-  return del(['./build/*'])
-}
-//Добавить доп. сайты в build
-function addOtherSite() {
-  return gulp.src('./src/other-site/*/*')
-  .pipe(gulp.dest('./build'))
+// Функция обработки картинок
+async function img(src, dest) {
+  return await gulp
+    .src(src)
+    .pipe(gulp.dest(dest))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
-//Просматривать файлы
-function watch() {
+// Собираем проект
+async function buildProject() {
+  // Обработка всех страниц (html, scss, js)
+  const arrayPages = fs.readdirSync(path.join(__dirname, "src/pages"));
+  let pathBuild = "";
+  for (let folder of arrayPages) {
+    if (folder === "main") {
+      pathBuild = "build";
+    } else {
+      pathBuild = `build/pages/${folder}`;
+    }
+    await html(
+      path.join(__dirname, `src/pages/${folder}/index.html`),
+      path.join(__dirname, `${pathBuild}`)
+    );
+    await scss(
+      path.join(__dirname, `src/pages/${folder}/style.scss`),
+      path.join(__dirname, `${pathBuild}`)
+    );
+    await js(
+      path.join(__dirname, `src/pages/${folder}/index.js`),
+      path.join(__dirname, `${pathBuild}`)
+    );
+  }
+
+  // Обработка стилей компонентов
+  await scss("./src/components/common.scss", "./build");
+  // Обработка картинок
+  await img("./src/static/images/*", "./build/static/images");
+  // Обработка шрифтов
+  await fonts("./src/static/fonts/*", "./build/static/fonts");
+}
+
+// Перебрасываем доп. сайты в папку "build"
+async function siteTransfer() {
+  return await gulp
+    .src("./src/others-sites/**")
+    .pipe(gulp.dest("./build/others-sites"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+// Функция которая слушает изменения файлов в проекте
+async function watch() {
   browserSync.init({
     server: {
       baseDir: "./build"
     }
   });
-  //Следить за SCSS файлами
-  gulp.watch('./src/main-site/scss/**/*.scss', scss)
-  //Следить за JS файлами
-  gulp.watch('./src/main-site/js/*.js', js)
-  //При изменении HTML запустить синхронизацию
-  gulp.watch([
-    "./src/main-site/html/components/*.html",
-    "./src/main-site/html/pages/*.html"
-  ], html).on('change', browserSync.reload);
+  // При изменении любого типа файлов пересобрать проект
+  gulp.watch("./src/**/*.*", buildProject);
 }
 
-gulp.task('scss', scss);
-gulp.task('js', js);
-gulp.task('html', html);
-gulp.task('fonts', fonts);
-gulp.task('img', img);
-gulp.task('addOtherSite', addOtherSite);
-gulp.task('watch', watch);
+gulp.task("watch", watch);
+gulp.task("buildProject", buildProject);
+gulp.task("siteTransfer", siteTransfer);
 
-//Запуск buld проекта
-gulp.task('build', gulp.series(clean, gulp.parallel(scss, js, html, fonts, img, addOtherSite)))
-//Запуск dev проекта
-gulp.task('dev', gulp.series('build', 'watch'));
+// Сборка проекта
+gulp.task("build", gulp.series(clean, buildProject, siteTransfer));
+// Режим разработки
+gulp.task("dev", gulp.series("build", "watch"));
